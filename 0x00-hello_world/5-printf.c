dd2dccb5dcd54831c92b4c7c49cb7926d01c58c4
Tednic <replyoku@gmail.com>
2023-06-17 06:42:58 -0700

    printf c prigram

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 int main(void)
 {
 	/* Display the character string inside the quotes*/
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
