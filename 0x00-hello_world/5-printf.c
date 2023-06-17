29f758402032e1f20d1b8ed91c5df918fb6a4c8f
Tednic <replyoku@gmail.com>
2023-06-17 06:36:11 -0700

    printf commsnd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - A C Program
+ * A Task using the printf function
+ * Return: 0 if Successful else 1 for error
+ */
+
+int main(void)
+{
+	/* Display the character string inside the quotes*/
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
