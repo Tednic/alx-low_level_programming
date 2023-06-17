52b69744446f3ad0455d1b3c4418163696452c8f
Tednic <replyoku@gmail.com>
2023-06-17 03:16:31 -0700

    my first c program edited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-int main(void)
+/**
+ * my_puts - Entry Point
+ * my_puts - Write a string to the output(stdout) stream
+ * 'C Program That Prints A Character String Using Puts functuon'
+ * Return - Always 0 (Success)
+ */
+
+void  my_puts(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
