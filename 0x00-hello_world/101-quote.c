f2b6396b82525d3881c438fbcf4f3a27060f3203
Tednic <replyoku@gmail.com>
2023-06-18 05:42:35 -0700

    c program without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,21 @@
-#!/bin/bash
+#include <unistd.h>
+/**
+  * main - C Program to print a comment
+  * Don't use any functions in man (3) printf or man (3) puts
+  * followed by a new line, to the standard error(stderr)
+  * program should return 1
+  * program should compile without any warnings when using the -Wall gcc option
+  * Return: 1
+  */
+
+int main(void)
+{
+	/**
+	  *2 is the default file descriptor for stderr
+	  * 59 is the size of string to be printed
+	  */
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
