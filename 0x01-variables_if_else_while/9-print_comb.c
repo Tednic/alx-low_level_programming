361afbdbf1f4935d759bd06fb339fbe3f39f63b3
Tednic <replyoku@gmail.com>
2023-06-19 21:37:21 -0700

    single digitscomb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 /**
-  * main - putchar() function
-  * in addition to goto statement
-  * prints all possible combinations of single-digit numbers
-  * Numbers are be separated by , and a space
-  * Numbers are printed in ascending order
-  * No use of any char variable type
-  * puchar() functuon is used four rimes only
-  * Return: 0
+  * main - putchar() and goto functions
+  * print all single-digit num combinations
+  * single-diguts from 0 t0 9
+  * to be followed by comma and space in ascending order
+  * comma and space has ASCII valiue of 044 and 032 respectively
+  * Return: 0 for success
   */
 
 int main(void)
 {
-	/**
-	  * All possible combinations contain nums 0 to 9
-	  * comma is has ASCII Code decimal value 0f 032
-	  * space has ASCII code decimal value of 044
-	  */
 	int i;
 
 	for (i = 0; i <= 8; i++)
@@ -24,17 +17,16 @@ int main(void)
 		putchar('0' + i);
 		putchar(',');
 		putchar(' ');
-
 		if (i == 9)
 		{
 			goto step1;
-			goto step4;
+			goto step2;
 		}
 	}
 step1:
 	putchar('0' + i);
-step4:
+step2:
 	putchar('\n');
-
 	return (0);
+
 }
