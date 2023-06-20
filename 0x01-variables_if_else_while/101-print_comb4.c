73e51982a698fe42fee374f1b91e5c47f33375cd
Tednic <replyoku@gmail.com>
2023-06-20 12:17:59 -0700

    three diguts unique combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+  * main - putchar() function
+  * prints all possible different combinations of thre digit
+  * Numbers should be printed in ascending order, with two digits
+  * Digits a, b, and c creates repeated pattern in output for a nested loop
+  * Return: 0 for pass
+  */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = a + 1; b < 9; b++)
+		{
+			for(c = b + 1; c < 10; c++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(c + '0');
+
+				if (a == 7 && b == 8 && c ==9)
+					continue;
+			
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
