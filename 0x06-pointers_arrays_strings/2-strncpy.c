a5b107505a0358ea79e016c4dd023cbb160c0483
Tednic <replyoku@gmail.com>
2023-06-29 14:41:28 -0700

    strncopy two

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,7 +11,9 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	if (dest == NULL)
-		return (NULL);
+	return (NULL);
+
+	char *ptr = dest;
 
 	while (*src && n--)
 	{
@@ -20,6 +22,6 @@ char *_strncpy(char *dest, char *src, int n)
 		src++;
 	}
 	*dest = '\0';
-	return (dest);
+	return (ptr);
 
 }
