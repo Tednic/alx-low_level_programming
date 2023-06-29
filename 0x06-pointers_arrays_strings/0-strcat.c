a90522366da28ad256e9312fdd644b7be6923586
Tednic <replyoku@gmail.com>
2023-06-29 04:24:24 -0700

    strcat join two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,18 +1,20 @@
 #include "main.h"
-#include <string.h>
-#include <stdlib.h>
 
 /**
   *_strcat - function appends the copy of a given C-string to another string
   * @dest: the pointer to the destination array, contain a C string,
   * and should have enough buffer to contain the resulting concatenated string.
   * @src: the string to be appended. It shouldn't overlap the destination.
+  * Implementation of the function via the code is as follows:
+  * make "ptr" to point to the end of the dest string
+  * join the characters in src to the dest string
+  * dest string is terminated by null byte "\0"
   * Return: dest
   */
 
-char* _strcat(char* dest, const char* src)
+char *_strcat(char *dest, char *src)
 {
-	char* ptr = dest + strlen(dest);
+	char *ptr = dest + strlen(dest);
 
 	while (*src != '\0')
 	{
