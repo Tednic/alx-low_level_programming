0b27532e666c77bf209b05e453d36b51585192e9
Tednic <replyoku@gmail.com>
2023-06-30 13:02:19 -0700

    strncpy implementation

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,18 +10,18 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0;
+	int i = 0;
 
-	int src_len = 0;
+	int j = 0;
 
-	while (src[index++])
-		src_len++;
+	while (src[i++])
+		j++;
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
 
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
+	for (i = j; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
