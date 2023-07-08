c7974b28799fd18e9eac602f51d5e42e4f58ee3b
Tednic <replyoku@gmail.com>
2023-07-08 12:27:10 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _isupper - checks for uppercase letters of the alphabet characters, c
+  * @c: the letters of tge alphabet
+  * Return: 1 if letter is uppercase and 0 otherwise
+  */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+	/* ASCII Code is used here */
+
+}
