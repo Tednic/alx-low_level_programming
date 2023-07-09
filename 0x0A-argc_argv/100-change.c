d9288f694186aeb89cf690ff92e9455f4813bfec
Tednic <replyoku@gmail.com>
2023-07-09 07:27:27 -0700

    coins and cents

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,58 @@
+/*
+ * File: 100-change.c
+ * Writer: Umoren Oku
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the least amount of coins to
+ *        make change for an amount of money.
+ * @argc: The count of arguments passed to the program.
+ * @argv: An array of pointers to the arguments passed.
+ * Return: 1 if argument counts == 1. otherwise 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		coins++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+			continue;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+
+	printf("%d\n", coins);
+
+	return (0);
+}
