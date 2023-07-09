4806f7acc706df01eefd1c249ffec6ade42996b5
Tednic <replyoku@gmail.com>
2023-07-09 05:04:12 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-whatsmyname.c
+ * Writer: Oku Umoren
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints a program's with a new line.
+ * @argc: The count of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always o.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
