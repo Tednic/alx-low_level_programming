0a4dc6980b6f83d38498097d979edf9f503fb8d4
Tednic <replyoku@gmail.com>
2023-07-11 22:47:48 -0700

    malloc files

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+/*
+ * File: 100-argstostr.c
+ * Task: Write a function that concatenates all the arguments of your program.
+ * Function Prototype: char *argstostr(int ac, char **av)
+ */
+
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int arg, byte, index, size = ac;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			size++;
+	}
+
+	str = malloc(sizeof(char) * size + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	index = 0;
+
+	for (arg = 0; arg < ac; arg++)
+	{
+		for (byte = 0; av[arg][byte]; byte++)
+			str[index++] = av[arg][byte];
+
+		str[index++] = '\n';
+	}
+
+	str[size] = '\0';
+
+	return (str);
+}
