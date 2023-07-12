e39f315acf0e4e3b429b14ac43994eab3f2bf501
Tednic <replyoku@gmail.com>
2023-07-12 02:23:25 -0700

    concatenate string of all arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,38 +8,48 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
+ * argstostr - concatenates all arguments of a program
+ * @ac: the number of strings (arguments) passed to the program
+ * @av: array of pointers to the arguments passed to the pragram
+ * Return: a pointer to the new string. NULL if ac || av == 0 or failure.
  */
 
 char *argstostr(int ac, char **av)
 {
+	/*
+	 * *str: a pointer to a char string from concatenated arguments passed to the program
+	 * i: loop counter for each argument in the array vector
+	 * j: loop counter for each character in each of the argument passed
+	 * k: keeps track  in array of the position in str 
+	 * size: char size
+	 */
+
 	char *str;
-	int arg, byte, index, size = ac;
+	int i, j, k, size = ac;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (arg = 0; arg < ac; arg++)
+	for (i = 0; i < ac; i++)
 	{
-		for (byte = 0; av[arg][byte]; byte++)
+		for (j = 0; av[i][j]; j++)
 			size++;
 	}
 
+	/* use malloc to allocate memory to str */
 	str = malloc(sizeof(char) * size + 1);
 
 	if (str == NULL)
 		return (NULL);
 
-	index = 0;
+	k = 0;
 
-	for (arg = 0; arg < ac; arg++)
+	for (i = 0; i < ac; i++)
 	{
-		for (byte = 0; av[arg][byte]; byte++)
-			str[index++] = av[arg][byte];
+		for (j = 0; av[i][j]; j++)
+			str[k++] = av[i][j];
 
-		str[index++] = '\n';
+		str[k++] = '\n';
 	}
 
 	str[size] = '\0';
