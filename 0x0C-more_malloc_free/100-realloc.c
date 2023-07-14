112643e90b0baae1c185ed93bde98c3524670240
Tednic <replyoku@gmail.com>
2023-07-14 02:22:35 -0700

    function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,14 +1,15 @@
 /*
  * Task: Write a function to reallocate a memory block with malloc and free
  * File:
- * Prototype: void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+ * Prototype: void *_realloc(void *ptr,
+ * unsigned int old_size, unsigned int new_size)
  * The contents to be copied to the newly allocated space,
  * from the start of ptr up to the minimum of the old and new sizes
  * If new_size > old_size, the “added” memory should not be initialized
  * If new_size == old_size do not do anything and return ptr
- * If ptr is NULL, then the call is equivalent to malloc(new_size),
- * for all values of old_size and new_size
- * If new_size = zero, and ptr != NULL, then the call is == free(ptr). Return NULL
+ * If ptr is NULL, then the call ==  malloc(new_size)...
+ * ...for all values of old_size and new_size
+ * If new_size = zero, ptr != NULL, then the call is == free(ptr). Return NULL
 * Don’t forget to free ptr when it makes sense
  */
 
@@ -17,9 +18,10 @@
 
 /**
  * _realloc - reallocates a memory block
- * @ptr: pointer memory allocated previously with a call to malloc: malloc(old_size)
+ * @ptr: pointer memory allocated prior with a call to malloc: malloc(old_size)
  * @old_size: is the size, in bytes, of the allocated space for ptr
  * @new_size: the new size, in bytes of the new memory block
+ * Return: a pointer to the reallocated block of memory. NULL (see above).
  */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
