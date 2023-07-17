df7044bd6c3a724264e59a3a5fd66027b434b61f
Tednic <replyoku@gmail.com>
2023-07-17 21:17:43 +0100

    function-like macro SUM(x, y)

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,14 @@
+#ifndef SUM_OF_TWO_NUM_MAXRO_H
+#define SUM_OF_TWO_NUM_MAXRO_H
+
+/*
+ * Task: Write a function-like macro SUM(x, y) that...
+ * ...computes the sum of the numbers x and y
+ * Filename: 4-sum.h
+ * File Description: a header file for the functuon-like macro.
+ */
+
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
