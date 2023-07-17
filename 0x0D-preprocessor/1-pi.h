4a2d7a55c701238ccfcde9e16b6ca4aa03fde609
Tednic <replyoku@gmail.com>
2023-07-17 16:18:36 +0100

    PI macros

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * Task: Create a header file that defines a macro named PI
+ * as an abbreviation for the token 3.14159265359
+ * File: 1-pi.h
+ */
+
+#define PI 3.14159265359
+
+#endif
