cb200b01bbe80482ef7e6a403486f48e55cffde5
Tednic <replyoku@gmail.com>
2023-07-17 22:21:38 +0100

    readme struct typedef folder and dog header

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,35 @@
+#ifndef DOG_H
+#define DOG_H
+
+/* 
+ * Task: Define a new type struct dog with the following elements:
+ * name, type = char *
+ * age, type = float
+ * owner, type = char *
+ * Filename & description: dog.h & A header file that defines a new type struct dog.
+ */
+
+/** 
+ * struct dog - A new type structure for a dog with various items.
+ * @name: The dog's name.
+ * @age: The dogs age.
+ * @owner: The dog's owner details.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
