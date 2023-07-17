6a74267c5f618a14d3facb14c5dc067539aa9d01
Tednic <replyoku@gmail.com>
2023-07-17 20:59:50 +0100

    function-like macro computes absolute value for X

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,13 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) ((x) >= (0) ? (x) : -(x))
+/*
+ * Task: Write a function-like macro ABS(x) that...
+ * ...computes the absolute value of a number x.
+ * Filename: 3-function_like_macro.h
+ * File Descriptuon: a header file tgat defines the function-like macro.
+ */
+
+#define ABS(x) (((x) >= (0)) ? (x) : -(x))
 
 #endif
