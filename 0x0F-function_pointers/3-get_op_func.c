1dc0db3993ca3e4887f184c20267674868c9196b
Tednic <replyoku@gmail.com>
2023-07-19 19:32:40 +0100

    program that performs simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,19 +1,7 @@
-/*
- * Task: 
- * Filename:
- */
-
 #include "3-calc.h"
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-/**
- * get_op_func - the function that selects the correct function to perform...
- * ...the operation asked by the user
- * @s: the operator passed as argument to the program
- * Return: A pointer to a function
- */
-int (*get_op_func(char *s))(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
@@ -23,15 +11,10 @@ int (*get_op_func(char *s))(int a, int b)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
+	int i = 0;
 
-	int i;
-
-	i = 0;
-	while (ops[i].op != NULL)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
-	return (NULL);
+
+	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,18 +1,16 @@
-/*
- * Task: Write a program that performs simple operations
- * Filename: 3-op_functions.c
- * This file should contain the 5 following functions (not more):
- * op_add, op_sub, op_mul, op_div, and op_mod.
- */
+#include "3-calc.h"
 
-#include <stdlib.h>
-#include <stdio.h>
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
- * op_add - sum up two numbers a and b
- * @a: number 1 of integer type
- * @b: number 2 of integer type
- * Return: The sum of numbers a n b
+ * op_add - performs the addition of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the sum of ad b
  */
 
 int op_add(int a, int b)
@@ -21,10 +19,10 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - Computes the difference of two numebers a and b
- * @a: number 1 of integer value
- * @b: number 2 of interger value
- * Return: The difference of numbers and b
+ * op_sub - performs the substraction of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the difference of a and b
  */
 
 int op_sub(int a, int b)
@@ -33,10 +31,10 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - Computes the product of two numebers a and b
- * @a: number 1 of integer type
- * @b: number 2 of integer type
- * Return: The product of numbers a and b
+ * op_mul - performs the multiplication of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the product of a and b
  */
 
 int op_mul(int a, int b)
@@ -45,36 +43,37 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - Computes the division of two numbers a and b
- * @a: number 1 of integer type
- * @b: number 2 of integer type
- * Return: The result of the division of numbers a and b
+ * op_div - performs the division of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the result of the division of a by b
  */
 
 int op_div(int a, int b)
 {
-/* if user divides (/) by 0, print Error with \n. Exit with status 100 */
 	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
 	return (a / b);
 }
 
 /**
- * op_mod - Computes  the remainder of two numbers
- * @a: number 1 of integer type
- * @b: number 2 of integer type
- * Return: The result of the remainder of numbers a and b
+ * op_mod - performs the division of a and b
+ * @a: number 1
+ * @b: number 2
+ * Return: the remainder of the division of a by b
  */
+
 int op_mod(int a, int b)
 {
-/* Print Error with \n. Exit with status 100 if user divide (%) by 0 */
 	if (b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
+
 	return (a % b);
 }
