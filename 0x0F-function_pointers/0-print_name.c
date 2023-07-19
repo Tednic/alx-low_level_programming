3acf2518ad2e11b2931cfc784a7e291ebe6faab5
Tednic <replyoku@gmail.com>
2023-07-19 08:05:10 +0100

    Write a function that prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,24 @@
+/*
+ * Task: Write a function that prints a name.
+ * Filename: 0-print_name.c
+ * Prototype: void print_name(char *name, void (*f)(char *))
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name
+ * @name: pointer to a name string containing tye name of the person
+ * @f: pointer to a functuon
+ * Return: Always 0 (Success)
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+		void (*prt_name)(char *);
+
+		if (f == NULL || name == NULL)
+			return;
+		prt_name = f;
+		prt_name(name);
+}
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdlib.h> 
+/** 
+ *   * array_iterator - Iterates through an array and calls a function 
+ *     * @array: Array to iterate 
+ *       * @size: size of the array 
+ *         * @action: Pointer to a function 
+ *           */ 
+void array_iterator(int *array, size_t size, void (*action)(int)) 
+{ 
+	unsigned int i; 
+
+	if (array == NULL || action == NULL) 
+		return; 
+
+	for (i = 0; i < size; i++) 
+		action(array[i]); 
+}
