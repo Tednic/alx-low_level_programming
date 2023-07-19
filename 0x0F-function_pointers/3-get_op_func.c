c3518260168f93638da720abcd7f31cbc1d74049
Tednic <replyoku@gmail.com>
2023-07-19 14:14:23 -0700

    program to peform simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,14 @@
 #include "3-calc.h"
 #include <stdlib.h>
 
+/**
+  * get_op_func - function that selects the correct function
+  * to perform the operation asked by the user.
+  * @s: the operator passed as argument to the program
+  * Return: pointer to the function that corresponds to the
+  * operator given as a parameter
+  */
+
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
