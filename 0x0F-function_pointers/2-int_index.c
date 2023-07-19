b18acdc27f53bf88aebfb3661eed41072c7bc6a3
Tednic <replyoku@gmail.com>
2023-07-19 10:05:12 +0100

    Write a function that searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,35 @@
-#include <stdlib.h> 
-/** 
- *   * int_index - Searches for an integer index on an array 
- *     * @array: Array containing the values 
- *       * @size: Size of the array 
- *         * @cmp: Pointer to a function 
- *           * Return: -1 if not index found or size is 0 or less. Index number when 
- *             * number is found 
- *               */ 
-int int_index(int *array, int size, int (*cmp)(int)) 
-{ 
-	int i; 
+/*
+ * Task: Write a function that searches for an integer
+ * Filename: 2-int_index.c
+ * Prototype: int int_index(int *array, int size, int (*cmp)(int))
+ */
 
-	if (array == NULL || cmp == NULL) 
-		return (-1); 
-	if (size <= 0) 
-		return (-1); 
-	for (i = 0; i < size; i++) 
-	{ 
-		if (cmp(array[i]) != 0) 
-			return (i); 
-	} 
-	return (-1); 
+#include <stdlib.h>
+
+/**
+ * int_index - a function that searches for an integer
+ * @size: the number of elements in the array array
+ * @cmp: pointer to the function to be used to compare values
+ * Return: returns the index of the first element for which
+ * the cmp function does not return 0. If no element matches, return -1
+ * If size <= 0, return -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+/* Use j to track the element that is an integer in the array */
+	int j;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (j = 0; j < size; j++)
+	{
+		if (cmp(array[j]) != 0)
+			return (j);
+	}
+	return (-1);
 }
