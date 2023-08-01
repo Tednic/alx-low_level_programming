d909fbfe43a192a8426d1e03abcb939796996b5c
Tednic <replyoku@gmail.com>
2023-08-01 07:19:47 +0000

    9-insert_nodeint.c: function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,32 +11,41 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *duplicate = *head;
+	listint_t *new_node, *temp, *temp_old;
 	unsigned int node;
+
+	temp = *head;
+	if (head == NULL)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->next = NULL;
+	return (new_node);
 
-	if (idx == 0)
+	if (*head == NULL)
 	{
-		new_node->next = duplicate;
 		*head = new_node;
 		return (new_node);
 	}
 
-	for (node = 0; node < (idx - 1); node++)
+	if (idx == 0)
+		*head = new_node;
+	for (node = 0; node < idx - 1 && temp != NULL && idx != 0; node++)
+
+		temp = temp->next;
+	if (temp == NULL)
+		return (NULL);
+	if (idx == 0)
+		new_node->next = temp;
+	else
 	{
-		if (duplicate == NULL || duplicate->next == NULL)
-			return (NULL);
-
-		duplicate = duplicate->next;
+		temp_old = temp->next;
+		temp->next = new_node;
+		new_node->next = temp_old;
 	}
-
-	new_code->next = duplicate->next;
-	duplicate->next = new_node;
-
 	return (new_node);
 }
