10a8293e460b9044e7f91d5d00ed8f7e725b2ac0
Tednic <replyoku@gmail.com>
2023-08-03 18:10:22 +0000

    function that checks the endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -8,10 +8,11 @@
  * Return: 0 if big endian, 1 if little endian
  */
 
-int get_endianness(void) 
-{ 
+int get_endianness(void)
+{
 	int num = 1; /*create an int with value 1*/
 	char *endian = (char *)&num; /*cast it to a char pointer*/
+
 	if (*endian == 1) /* if first byte is 1*/
 		return (1); /*return 1 (little endian)*/
 	return (0); /*else return 0 (Big endian)*/
